0efaac890674877cec13f6479db74c578f032cd3
Reha <66319535+Rehasree@users.noreply.github.com>
2023-01-24 23:42:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ def sms():
   response = "hi, how you doin"
 
   # Twilio account information
-  account_sid = 'AC3e6f069fc91041066f0f8f8d21a39780'
-  auth_token = '72f15924d14fa7ea6dfea469a3c79b5a'
+  account_sid = os.environ['TWILIO_ACCOUNT_SID']
+  auth_token = os.environ['TWILIO_AUTH_TOKEN']
   client = Client(account_sid, auth_token)
  
 
